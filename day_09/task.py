abcfe50c1fb42c5f745291903c2bacaccd6ee16b
Lachezar Bozhkov <lachezar.bozhkov@gmail.com>
2020-12-09 17:51:49 +0200

    Add and format day 09 solutions

--- day_09/task.py
+++ day_09/task.py
@@ -8,48 +8,56 @@ def read_input(file_name="input.txt"):
     return lines
 
 
-def parse_input(lines):
-    lines = [line.split() for line in lines]
-    return lines
+def task_1(lines, previous):
+    for i in range(previous, len(lines)):
+        previous_lines = lines[i - previous : i]
+        current_number = lines[i]
+        is_valid = False
+
+        for j in range(len(previous_lines)):  # Iterate through all possible lines
+            # k & j will try all possible previous numbers combinations
+            for k in range(j + 1, len(previous_lines)):
+                is_valid = current_number == (previous_lines[k] + previous_lines[j])
+                if is_valid:
+                    break
+            if is_valid:
+                break
+
+        if is_valid is False:
+            return i, current_number
+
+    return -1, "All valid numbers"
+
+
+def task_2(lines, index_number):
+    for i in range(index_number):
+        for j in range(i + 1, index_number):
+            if sum(lines[i:j]) == lines[index_number]:
+                return min(lines[i:j]) + max(lines[i:j])
 
 
 if __name__ == "__main__":
     print("## --- Solution ---")
 
     # Tests
-    lines = read_input()
-    # print(lines)
-
-    previous = 25
-    for i in range(previous, len(lines)):
-        temp_lines = lines[i - previous : i]
-        current_number = int(lines[i])
-        # print(current_number, temp_lines)
-        number_ok = False
-        for j in range(len(temp_lines)):
-            for k in range(j + 1, len(temp_lines)):
-                sum_numbers = int(temp_lines[k]) + int(temp_lines[j])
-                number_ok = (sum_numbers == current_number) or number_ok
-                if number_ok:
-                    break
-            if number_ok:
-                break
-
-        if number_ok is False:
-            print("!!! Number not ok")
-            print(i, current_number)
-            index_number = i
-            break
-
-    print(index_number, current_number)
-
+    lines = read_input("test.txt")
     lines = [int(line) for line in lines]
-    for i in range(index_number):
-        for j in range(i + 1, index_number):
-            if sum(lines[i:j]) == current_number:
-                print("This", i, j, sum(lines[i:j]))
-                print(min(lines[i:j]) + max(lines[i:j]))
-                exit()
-        # else:
-        #     print("Number ok!")
-        #     print(i, lines[i])
+    previous = 5
+    index_number, number = task_1(lines, previous)
+    assert number == 127
+    assert index_number == 14
+
+    min_plus_max = task_2(lines, index_number)
+    assert min_plus_max == 62
+
+    # Real input
+    lines = read_input()
+    lines = [int(line) for line in lines]
+
+    # Task 1
+    previous = 25
+    index_number, number = task_1(lines, previous)
+    print("Task 1 index:", index_number, "number:", number)
+
+    min_plus_max = task_2(lines, index_number)
+    print("Task 2:", min_plus_max)
